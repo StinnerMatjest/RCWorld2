4fc72b1a0754b015451c9398edba0a8f0fdfb918
StinnerMatjest <horm@live.dk>
2025-01-14 13:59:55 +0100

    WORK

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['img.daisyui.com'],
   },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.optimization.splitChunks.maxSize = 200000;  // 200KB
-    }
-    config.devtool = false;  // Disable source maps for production
-    return config;
-  },
-  // You can add other production-specific settings here if needed
 };
+
 
 export default nextConfig;
