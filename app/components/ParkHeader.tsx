7a0973cf5bc025fd0521a3816ab971f0f6cec689
JannikIbsen <jannikibsen@hotmail.com>
2025-07-20 14:13:28 +0200

    Removed overlay + image size

--- app/components/ParkHeader.tsx
+++ app/components/ParkHeader.tsx
@@ -7,7 +7,7 @@ interface ParkHeaderProps {
 }
 
 const ParkHeader: React.FC<ParkHeaderProps> = ({ park }) => (
-<div className="relative w-full aspect-[16/8] md:aspect-[16/4] max-h-screen overflow-hidden">
+<div className="relative w-full aspect-[16/9] md:aspect-[16/4] max-h-screen overflow-hidden">
     <Image
       src={park.imagepath}
       alt={park.name}
@@ -16,12 +16,8 @@ const ParkHeader: React.FC<ParkHeaderProps> = ({ park }) => (
       priority
       unoptimized
     />
-
-    {/* Overlay */}
-    <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/30 to-transparent z-10" />
-
-    {/* Title */}
-    <h1 className="absolute bottom-6 left-6 text-3xl md:text-5xl font-bold text-white z-20 drop-shadow-lg">
+  {/* Title */}
+<h1 className="absolute bottom-6 left-6 text-4xl md:text-5xl font-bold text-white z-20 drop-shadow-lg">
       {park.name}
     </h1>
   </div>
