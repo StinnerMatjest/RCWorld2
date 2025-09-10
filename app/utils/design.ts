fed4ad7ff9ddfdff4a43b3176129b7437ab0e784
StinnerMatjest <horm@live.dk>
2025-09-10 19:08:45 +0200

    Stuffz

--- app/utils/design.ts
+++ app/utils/design.ts
@@ -1,12 +1,21 @@
-export const getRatingColor = (rating: number): string => {
-  if (rating >= 10) return "rainbow-animation";
-  if (rating >= 9) return "text-blue-700 dark:text-blue-400";
+export const getRatingColor = (rating: number | string) => {
+  if (
+    rating === "" ||
+    rating === "Select Rating" ||
+    typeof rating !== "number"
+  ) {
+    return "text-black dark:text-gray-100";
+  }
+
+  if (rating >= 10.0) return "rainbow-animation";
+  if (rating >= 9.0) return "text-blue-700 dark:text-blue-400";
   if (rating >= 7.5) return "text-green-600 dark:text-green-400";
   if (rating >= 6.5) return "text-green-400 dark:text-green-300";
   if (rating >= 5.5) return "text-yellow-400 dark:text-yellow-300";
   if (rating >= 4.5) return "text-yellow-600 dark:text-yellow-500";
-  if (rating >= 3) return "text-red-400 dark:text-red-300";
-  return "text-red-600 dark:text-red-500";
+  if (rating >= 3.0) return "text-red-400 dark:text-red-300";
+  if (rating <= 2.9) return "text-red-600 dark:text-red-500";
+  return "text-black dark:text-gray-100";
 };
 
 const countryNameToCode: Record<string, string> = {
@@ -66,4 +75,3 @@ export const getParkFlag = (countryName: string): string => {
   }
   return `https://flagcdn.com/w40/${code}.png`;
 };
-
